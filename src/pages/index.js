3ee2a350f4a4efcbd59cac36f0c65caff45be59d
kiranojhanp <kiranojha1226@gmail.com>
2020-09-10 09:52:00 +0545

    Initital setup

---
+++ src/pages/index.js
@@ -0,0 +1,15 @@
+import React from "react"
+import 'bootstrap/dist/css/bootstrap.min.css';
+import {Button} from 'reactstrap';
+
+const Index = () => {
+  return (
+    <div>
+      <h1>Home page</h1>
+      <h2>I'm Ibas, a teacher and a Gatsby.js developer</h2>
+      <Button color="success">Hire me</Button>
+    </div>
+  )
+}
+
+export default Index
